9eced66b86ff19a7131e4713b3c31606bbba3f69
kentaroy47 <kentaroy@stanford.edu>
2022-03-12 21:23:30 +0900

    add cait trainsettings

--- train_cifar10.py
+++ train_cifar10.py
@@ -149,7 +149,21 @@ elif args.net=="vit_timm":
     import timm
     net = timm.create_model("vit_base_patch16_384", pretrained=True)
     net.head = nn.Linear(net.head.in_features, 10)
-
+elif args.net=="cait":
+    from models.cait import CaiT
+    net = ViT(
+    image_size = size,
+    patch_size = args.patch,
+    num_classes = 10,
+    dim = int(args.dimhead),
+    depth = 6,   # depth of transformer for patch to patch attention only
+    cls_depth=2, # depth of cross attention of CLS tokens to patch
+    heads = 8,
+    mlp_dim = 512,
+    dropout = 0.1,
+    emb_dropout = 0.1,
+    layer_dropout = 0.05
+)
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net) # make parallel
