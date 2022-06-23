ad0e19a923539086ee83f42e7fa35c0d3a7a44f3
Kentaro Yoshioka <meathouse47@gmail.com>
2022-06-23 09:44:24 +0900

    Update train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -32,7 +32,7 @@ from models.convmixer import ConvMixer
 
 # parsers
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=1e-3, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4?
+parser.add_argument('--lr', default=1e-4, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4
 parser.add_argument('--opt', default="adam")
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--noaug', action='store_true', help='disable use randomaug')
